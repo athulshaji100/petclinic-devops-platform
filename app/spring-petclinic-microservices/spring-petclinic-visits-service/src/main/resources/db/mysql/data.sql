3ad7644dddeb83b417f8667a7f2d2662d7fe04aa
athulshaji100 <athulshaji100@gmail.com>
2026-06-20 18:42:03 +0100

    db script updated

--- app/spring-petclinic-microservices/spring-petclinic-visits-service/src/main/resources/db/mysql/data.sql
+++ app/spring-petclinic-microservices/spring-petclinic-visits-service/src/main/resources/db/mysql/data.sql
@@ -1,3 +1,5 @@
+USE petclinic;
+
 INSERT IGNORE INTO visits VALUES (1, 7, '2010-03-04', 'rabies shot');
 INSERT IGNORE INTO visits VALUES (2, 8, '2011-03-04', 'rabies shot');
 INSERT IGNORE INTO visits VALUES (3, 8, '2009-06-04', 'neutered');
